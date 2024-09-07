1e5c384fae394ab7307e77117cb69d11f143c0eb
lovrozagar <lovro.zagar5@gmail.com>
2024-09-07 14:13:25 -0700

    refactor: carousel

--- src/components/carousel/components/carousel-indicators.tsx
+++ src/components/carousel/components/carousel-indicators.tsx
@@ -2,10 +2,10 @@
 
 import { EMPTY_OBJECT } from '@/components/_shared/constants/constants'
 import { Button } from '@/components/button/components/button'
+import { carouselIndicatorClasses } from '@/components/carousel/classes/carousel-indicator-classes'
+import { carouselIndicatorContainerClasses } from '@/components/carousel/classes/carousel-indicator-container-classes'
 import { useCarouselContext } from '@/components/carousel/contexts/carousel-context'
 import type { CarouselIndicatorsProps } from '@/components/carousel/types/carousel-indicators'
-import { carouselIndicatorContainerClasses } from '@/components/carousel/classes/carousel-indicator-container-classes'
-import { carouselIndicatorClasses } from '@/components/carousel/classes/carousel-indicator-classes'
 import { chain, cn, polymorphic } from '@renderui/utils'
 
 const CarouselIndicators = (props: CarouselIndicatorsProps) => {
