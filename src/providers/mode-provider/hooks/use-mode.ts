46219a5fe7be719479f3d29471d6cfaca7df8a1d
lovrozagar <lovro.zagar5@gmail.com>
2024-10-13 03:39:16 -0700

    feat: add mode toggle

--- src/providers/mode-provider/hooks/use-mode.ts
+++ src/providers/mode-provider/hooks/use-mode.ts
@@ -1,11 +1,23 @@
 import { useTheme } from 'next-themes'
+import React from 'react'
 
 function useMode() {
 	const theme = useTheme()
 
+	const toggleMode = React.useCallback(() => {
+		if (theme.resolvedTheme === 'light') {
+			theme.setTheme('dark')
+
+			return
+		}
+
+		theme.setTheme('light')
+	}, [theme.resolvedTheme, theme.setTheme])
+
 	return {
 		mode: theme.resolvedTheme,
 		setMode: theme.setTheme,
+		toggleMode,
 		modes: theme.themes,
 		forcedMode: theme.forcedTheme,
 		systemMode: theme.systemTheme,
